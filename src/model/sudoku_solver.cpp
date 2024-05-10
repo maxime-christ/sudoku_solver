f7a4694273c07707d54533471cbc78769196be51
Maxime Christ <contact@maximechrist.fr>
2024-05-10 19:22:21 +0200

    Solver: add csv parser.

--- src/model/sudoku_solver.cpp
+++ src/model/sudoku_solver.cpp
@@ -1,11 +1,38 @@
 #include <cassert>
+#include <fstream>
+#include <sstream>
 #include "sudoku_solver.h"
 
 void SudokuSolver::init_from_file(std::string& filename)
 {
     std::cout << "Reading file " << filename << std::endl;
+    std::ifstream input {filename};
 
-    // Loop through CSV and push_back events to constraint_event_list
+    if (!input.is_open())
+    {
+        std::cerr << "Couldn't read file!" << std::endl;
+        std::exit(1);
+    }
+
+    int i = 0;
+    int x, y;
+    std::string line;
+    while(std::getline(input, line))
+    {
+        std::istringstream ss(std::move(line));
+        std::string token;
+        while(std::getline(ss, token, ','))
+        {
+            if (!token.empty())
+            {
+                x = i / 9;
+                y = i % 9;
+                std::cout << "Token at (" << x << ',' << y << ") is " << token << std::endl;
+            }
+
+            i++;
+        }
+    }
 }
 
 void SudokuSolver::solve()
