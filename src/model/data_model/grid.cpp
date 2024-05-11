f2dc17bcc685b102eb420a24da153cd1ab00f83d
Maxime Christ <contact@maximechrist.fr>
2024-05-11 18:22:40 +0200

    Grid: write proper constructor and destructor.

--- src/model/data_model/grid.cpp
+++ src/model/data_model/grid.cpp
@@ -1,4 +1,60 @@
 #include "grid.h"
+#include <vector>
+
+Grid::Grid() : solved(false)
+{
+    std::array<std::set<Cell*>,9> cell_ptr_rows;
+    std::array<std::set<Cell*>,9> cell_ptr_columns;
+    std::array<std::set<Cell*>,9> cell_ptr_squares;
+
+    int square_index;
+    for(int i = 0; i < 9; i++)
+    {
+        for(int j = 0; j < 9; j++)
+        {
+            Cell* cell_ptr = &cells[i][j];
+            cell_ptr_rows[i].insert(cell_ptr);
+            cell_ptr_columns[j].insert(cell_ptr);
+
+            square_index = (i/3 * 3) + j/3;
+            cell_ptr_squares[square_index].insert(cell_ptr);
+        }
+    }
+
+    for(auto cell_ptr_row: cell_ptr_rows)
+    {
+        Container* row = new Container(cell_ptr_row);
+        rows.push_back(row);
+        for(auto cell_ptr: cell_ptr_row)
+            cell_ptr->setRow(row);
+    }
+
+    for(auto cell_ptr_col: cell_ptr_columns)
+    {
+        Container* column = new Container(cell_ptr_col);
+        columns.push_back(column);
+        for(auto cell_ptr: cell_ptr_col)
+            cell_ptr->setColumn(column);
+    }
+
+    for(auto cell_ptr_square: cell_ptr_squares)
+    {
+        Container* square = new Container(cell_ptr_square);
+        squares.push_back(square);
+        for(auto cell_ptr: cell_ptr_square)
+            cell_ptr->setSquare(square);
+    }
+}
+
+Grid::~Grid()
+{
+    for(auto row: rows)
+        delete row;
+    for(auto column: columns)
+        delete column;
+    for(auto square: squares)
+        delete square;
+}
 
 std::ostream & operator<<(std::ostream& stream, Grid const& grid)
 {
--- src/model/data_model/grid.h
+++ src/model/data_model/grid.h
@@ -2,6 +2,8 @@
 
 #include <iostream>
 #include <array>
+#include <vector>
+
 #include "cell.h"
 
 typedef std::array<std::array<Cell, 9>, 9> CellGrid;
@@ -9,13 +11,17 @@ typedef std::array<std::array<Cell, 9>, 9> CellGrid;
 class Grid
 {
 public:
-    Grid() : solved(false) {};
+    Grid();
+    ~Grid();
     inline bool is_solved() const { return solved; };
     inline CellGrid getCells() const { return cells; }
     inline void solve() { solved = true; };
 private:
     bool solved;
     CellGrid cells;
+    std::vector<Container*> rows;
+    std::vector<Container*> columns;
+    std::vector<Container*> squares;
 };
 
 std::ostream & operator<<(std::ostream& stream, Grid const& grid);
