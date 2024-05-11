268761b07bf7a4bf610ae9e4e1739826d4154e3e
Maxime Christ <contact@maximechrist.fr>
2024-05-11 13:39:22 +0200

    Solver: better handling of method visibility.

--- src/solver/sudoku_solver.h
+++ src/solver/sudoku_solver.h
@@ -9,13 +9,19 @@
 class SudokuSolver
 {
 public:
+    // User exposed API
     void init_from_file(std::string& filename);
     void solve();
     Grid solution() const;
 
+protected:
+    // Command related API
     void setCellValue(int& x, int& y, int value);
+
 private:
     std::vector<SetCellValueCommand> constraints_cmd_list;
     std::vector<Command> incidential_cmd_list;
     Grid grid;
+
+    friend SetCellValueCommand;
 };
